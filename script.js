447f356922b616b3400858b884abf291ce639835
Anton <160518473+tshnepes@users.noreply.github.com>
2024-03-11 18:49:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+//кнопки
+const breakfastButton = document.createElement('button');
+breakfastButton.textContent = 'Завтрак';
+document.body.append(breakfastButton);
 
+const lunchButton = document.createElement('button');
+lunchButton.textContent = 'Обед';
+document.body.append(lunchButton);
+
+const dinnerButton = document.createElement('button');
+dinnerButton.textContent = 'Ужин';
+document.body.append(dinnerButton);
+
+// клики
+breakfastButton.addEventListener('click', () => {
+  // Действие при клике на кнопку "Завтрак"
+  console.log('проверка"');
+});
+
+lunchButton.addEventListener('click', () => {
+  // Действие при клике на кнопку "Обед"
+  console.log('проверка"');
+});
+
+dinnerButton.addEventListener('click', () => {
+  // Действие при клике на кнопку "Ужин"
+  console.log('проверка"');
+});
